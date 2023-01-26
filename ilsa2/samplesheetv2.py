46a69f7b008428afd48e19906af05755fc5ee9db
Jakob Simeth <jakob.simeth@physik.uni-regensburg.de>
2023-01-26 07:07:04 +0100

    implement some logic.

--- ilsa2/samplesheetv2.py
+++ ilsa2/samplesheetv2.py
@@ -2,13 +2,14 @@ from ilsa2.sectionedsheet import SectionedSheet, settings_to_string, data_to_str
 from jsonschema import validate
 import re
 from collections import OrderedDict
+import types
 #
 # a schema that validates a sectioned sheet to be a samplesheet
 # we will put this elsewhere, but for now this is the place:
 # this follows
 # https://support-docs.illumina.com/IN/NextSeq10002000/Content/SHARE/SampleSheetv2/SampleSheetValidation_fNS_m2000_m1000.htm
 # (which is not a proper spec, but reasonably close to it and this is my interpretation)
-samplesheetv2schema = {
+illuminasamplesheetv2schema = {
     "type": "object",
     "required": ["Header", "Reads", "Sequencing_Settings"],
     "properties": {
@@ -153,8 +154,97 @@ samplesheetv2schema = {
             }
         }
     }
-
 }
+def parse_overrideCycles(cyclestr : str) -> dict:
+    def expand(short: str) -> str:
+        res = ""
+        pt = re.compile("([NYIU]+)([0-9]*);?")
+        matches = re.findall(pt, short)
+        for letter, freq in matches:
+            res += letter * int(freq)
+        return res
+
+    cycles = cyclestr.split(";")
+    if len(cycles) < 1:
+        raise Exception(f"OverrideCycles {cyclestr} cannot be parsed to a cycle sequence.")
+    res = {"Read1Cycles": expand(cycles[0])}
+    if( len(cycles) == 2 ):
+        res['Read2Cycles'] = expand(cycles[1])
+    elif( len(cycles) == 3 ):
+        res['Index1Cycles'] = expand(cycles[1])
+        res['Read2Cycles'] = expand(cycles[2])
+    elif( len(cycles) == 4 ):
+        res['Index1Cycles'] = expand(cycles[1])
+        res['Index2Cycles'] = expand(cycles[2])
+        res['Read2Cycles'] = expand(cycles[3])
+    elif( len(cycles) == 1 ):
+        pass
+    else:
+        raise Exception(f"OverrideCycles {cyclestr} defines too many elements.")
+    return res
+
+
+def illuminasamplesheetv2logic(doc : SectionedSheet):
+    """
+    this function checks the logic that is described in
+    https://support-docs.illumina.com/IN/NextSeq10002000/Content/SHARE/SampleSheetv2/SampleSheetValidation_fNS_m2000_m1000.htm
+
+    We won't check at the level of the index kits (i.e. that the indices match the kit etc)
+    """
+    if 'BCLConvert_Settings' in doc:
+        if 'OverrideCycles' in doc['BCLConvert_Settings']:
+            cycles = parse_overrideCycles(doc['BCLConvert_Settings']['OverrideCycles'])
+            for elemname, elemseq in cycles.items():
+                if elemname not in doc['Reads']:
+                    raise Exception(f"BCLConvert_Settings.OverrideCycles defines {elemname}, but it is not specified in the Reads section")
+                if doc['Reads'][elemname] != len(elemseq):
+                    raise Exception(f"Reads.{elemname} is {doc['Reads'][elemname]}, but BCLConvert_Settings.OverrideCycles specifies a length of {len(elemseq)}")
+            for elemname in [i for i in ['Read1Cycles', 'Read2Cycles', 'Index1Cycles', 'Index2Cycles'] if i in doc.keys()]:
+                if elemname not in cycles.keys():
+                    raise Exception(f"Reads defines {elemname}, but BCLConvert_Settings.OverrideCycles is incompatible with it.")
+        if 'AdapterRead1' in doc['BCLConvert_Settings']:
+            if len(doc['BCLConvert_Settings']['AdapterRead1']) > len(doc['Reads']['Read1Cycles']):
+                raise Exception(f"BCLConvert_Settings.AdapterRead1 is longer then Reads.Read1Cycles")
+        if 'AdapterRead2' in doc['BCLConvert_Settings']:
+            if 'Read2Cycles' not in doc['Reads']:
+                raise Exception('AdapterRead2 defined in BCLConvert_Settings, but no Read2Cycles entry in Reads')
+            if len(doc['BCLConvert_Settings']['AdapterRead2']) > len(doc['Reads']['Read2Cycles']):
+                raise Exception("BCLConvert_Settings.AdapterRead2 is longer then Reads.Read2Cycles")
+        # The "spec" says: "Only required if Index1Cycles is specified." but this conflicts with an default value of 1
+        # so I assume it must be the other way around: if there are Mismatches defined, then this must also be in the Reads section
+        if 'BarcodeMismatchesIndex1' in doc['BCLConvert_Settings']:
+            if 'Index1Cycles' not in doc['Reads']:
+                raise Exception("BCLConvert_Settings defines BarcodeMismatches1, but no Index1Cycles defined in Reads.")
+        if 'BarcodeMismatchesIndex2' in doc['BCLConvert_Settings']:
+            if 'Index2Cycles' not in doc['Reads']:
+                raise Exception("BCLConvert_Settings defines BarcodeMismatches2, but no Index2Cycles defined in Reads.")
+    if 'BCLConvert_Data' in doc:
+        # Sample_ID do not need to be unique?!
+        if( len(doc['BCLConvert_Data']) > 1 ):
+            if 'Index' not in doc['BCLConvert_Data'][0]:
+                raise Exception("No Index found in BCLConvert_Data, although it contains more than one sample")
+            index1 = [i['Index'] for i in doc['BCLConvert_Data']]
+            if 'Index2' in doc['BCLConvert_Data'][0]:
+                index2 = [i['Index2'] for i in doc['BCLConvert_Data']]
+                index = [i1+i2 for i1, i2 in zip(index1, index2)]
+            else:
+                index = index1
+            if len(set(index)) != len(index):
+                raise Exception("Indices are not unique.")
+
+def basespacelogic(doc: SectionedSheet):
+    if 'Cloud_Data' not in doc:
+        raise Exception("no Cloud_Data section")
+    if 'BCLConvert_Data' not in doc:
+        raise Exception("no BCLConvert_Data section")
+    cloud_sample_ids = [i['Sample_ID'] for i in doc['Cloud_Data']]
+    bclconvert_sample_ids = [i['Sample_ID'] for i in doc['BCLConvert_Data']]
+    for convert_id in bclconvert_sample_ids:
+        if convert_id not in cloud_sample_ids:
+            raise Exception(f"Sample_ID {convert_id} is defined in the BCLConvert_Data section, but not in the Cloud_Data section.")
+    # should we test also for the reverse?
+
+
 
 bclconvertschema = {
 }
@@ -178,16 +268,19 @@ nextseq1k2kschema = {
 }
 
 class SampleSheetV2:
-    def __init__(self, secsheet: SectionedSheet, schemata = [samplesheetv2schema]):
-        if schemata is None:
+    def __init__(self, secsheet: SectionedSheet, validation = [illuminasamplesheetv2schema, illuminasamplesheetv2logic]):
+        if validation is None:
             schemata = []
-        elif type(schemata) == list:
+        elif type(validation) == list:
             pass
         else:
-            schemata = [schemata]
+            validation = [validation]
 
-        for schema in schemata:
-            validate(instance=secsheet, schema=schema)
+        for schema in validation:
+            if(type(schema) == dict):
+                validate(instance=secsheet, schema=schema)
+            elif(type(schema) == types.FunctionType):
+                schema(secsheet)
 
         def secname(k):
             secsel = re.compile("^(.*)_(Settings|Data)$")
