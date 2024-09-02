0d6bfdd686df18ee5a22e21747dc5503b61964a5
Jakob Simeth <jakob.simeth@physik.uni-regensburg.de>
2024-09-02 09:11:07 +0200

    allow additional schema definition in CLI

--- src/samshee/__main__.py
+++ src/samshee/__main__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-from samshee.samplesheetv2 import read_samplesheetv2, parse_samplesheetv2_from_json
+from samshee.samplesheetv2 import SampleSheetV2, read_samplesheetv2, parse_samplesheetv2_from_json
 #from samshee.samplesheetv1 import read_samplesheetv1
 from samshee.sectionedsheet import read_sectionedsheet
+from samshee.validation import validate
 import sys
 import argparse
+import json
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
@@ -12,29 +14,45 @@ parser.add_argument(
     "infile",
     help="input sample sheet",
 )
-parser.add_argument("--output-format", help="print the read sheet in this format (possible values: v1, v2, sectioned, json)", default="v2")
-parser.add_argument("--input-format", help="expect the input file to be in this format (possible values: v2, json)", default="v2")
+parser.add_argument("--input-format", help="expect the input file to be in this format (possible values: sectioned, json) (v1 sheets can be read as sectioned sheets but are not validated by default!)", default="sectioned")
+parser.add_argument("--output-format", help="print the read sheet in this format (possible values: sectioned, v2, json).", default="v2")
+parser.add_argument("--schema", help="specifies an additional schema (other than those required by the input or output formats) in json format, e.g. \'{\"required\": [\"Header\", \"Reads\"]}\'. Use \"$ref\" for linking to predefined external or samshee-internal schemas, e.g., \'{\"$ref\": \"urn:samshee:illuminav2/v1\"}\'. This argument can be given more than once.", default=[], action="append")
 
 args = parser.parse_args()
 
 infile = Path(args.infile)
 
-if args.input_format == "json":
-    with infile.open("r") as fh:
-        jstr = fh.read()
-    sheet = parse_samplesheetv2_from_json(jstr)
-elif args.input_format == "v2":
-    sheet = read_samplesheetv2(infile)
-elif args.input_format == "sectioned":
-    sheet = read_sectionedsheet(infile)
-#elif args.input_format == "v1":
-#    sheet = read_samplesheetv1(infile)
-else:
-    raise ValueError(f"Unknown input format: {args.input_format}")
+try:
+    if args.input_format == "json":
+        with infile.open("r") as fh:
+            jstr = fh.read()
+        sheet = parse_samplesheetv2_from_json(jstr)
+    elif args.input_format == "sectioned":
+        sheet = read_sectionedsheet(infile)
+    else:
+        print(f"Unknown input format: {args.input_format}. Must be json or sectioned. Use \"sectioned\" for any v1 or v2 sample sheets.", file=sys.stderr)
+        sys.exit(3)
+except Exception as exc:
+    print(f"Error reading sheet: {exc}", file=sys.stderr)
+    sys.exit(1)
 
-if args.output_format == "json":
-    print(sheet.to_json(pretty=True))
-elif args.output_format == "v2":
-    print(sheet)
-else:
-    raise ValueError(f"Unknown output format: {args.output_format}")
+try:
+    validate(sheet, validation=[json.loads(schema) for schema in args.schema])
+except Exception as exc:
+    print(f"Validation failed: {exc}", file=sys.stderr)
+    sys.exit(2)
+
+
+try:
+    if args.output_format == "json":
+        print(sheet.to_json(pretty=True))
+    elif args.output_format == "v2":
+        print(SampleSheetV2(sheet))
+    elif args.output_format == "sectioned":
+        print(sheet)
+    else:
+        print(f"Unknown output format: {args.output_format}. Must be json, v2, or sectioned.", file=sys.stderr)
+        sys.exit(3)
+except Exception as exc:
+    print(f"failed writing output: {exc}", file=sys.stderr)
+    sys.exit(4)
